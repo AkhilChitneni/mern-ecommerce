67e59d39c05f4574a271db34672c620c139f1455
Akhil Chitneni <akhilchitneni@gmail.com>
2021-03-28 02:09:35 +0530

    product screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen/HomeScreen'
+import ProductScreen from './screens/ProductScreen/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
 
 const App = () => {
   return (
-    <div className="App">
-      <p> hello the game begins</p>
-    </div>
+    
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>   
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
